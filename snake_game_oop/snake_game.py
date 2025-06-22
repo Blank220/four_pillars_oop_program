815381faac02899217b22f905e3bcfd81024d198
Mark Comia <itzextinct2@gmail.com>
2025-06-22 14:33:40 +0800

    added the game size or features

--- snake_game_oop/snake_game.py
+++ snake_game_oop/snake_game.py
@@ -1,6 +1,15 @@
 from tkinter import *
 import random
 
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 100
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#FFFFFF"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR = "#000000"
+
 window = Tk()
 window.title("Snake game")
 window.resizable(False, False)
