61bb4b8c825456797d890498b235a73e65b14fa1
Mark Comia <itzextinct2@gmail.com>
2025-06-22 15:25:40 +0800

    made the snake class and list its properties

--- snake_game_oop/snake_game.py
+++ snake_game_oop/snake_game.py
@@ -10,6 +10,29 @@ SNAKE_COLOR = "#FFFFFF"
 FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
 
+class Snake:
+    def __init__(self):
+        self.body_size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
+
+    def move(self, x, y):
+        self.coordinates.insert(0, (x, y))
+        square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR)
+        self.squares.insert(0, square)
+
+    def remove_tail(self):
+        del self.coordinates[-1]
+        canvas.delete(self.squares[-1])
+        del self.squares[-1]
+
 window = Tk()
 window.title("Snake game")
 window.resizable(False, False)
